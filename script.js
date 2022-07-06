a18a4c9a4ba6c3dd6f21c07782eb279c02a170e2
Jane McPheron <jvmcpheron@Janes-MacBook-Pro.local>
2022-07-06 14:34:00 -0500

    clear planner button functionality

--- script.js
+++ script.js
@@ -49,3 +49,22 @@ document.getElementById("saturdayText").innerHTML = localStorage.getItem('saturd
 
 
 //CLEAR PLANNER FUNCTIONALITY
+
+let clearBtn = document.getElementsByClassName('btn-danger')[0];
+
+clearBtn.addEventListener("click" ,  () => {
+    localStorage.setItem('sundayText' , "");
+    localStorage.setItem('mondayText', "");
+    localStorage.setItem('tuesdayText' , "");
+    localStorage.setItem('wednesdayText', "");
+    localStorage.setItem('thursdayText', "");
+    localStorage.setItem('fridayText' , '');
+    localStorage.setItem('saturdayText' , "");
+    document.getElementById("sundayText").innerHTML = localStorage.getItem('sundayText');
+document.getElementById("mondayText").innerHTML = localStorage.getItem('mondayText');
+document.getElementById("tuesdayText").innerHTML = localStorage.getItem('tuesdayText');
+document.getElementById("wednesdayText").innerHTML = localStorage.getItem('wednesdayText');
+document.getElementById("thursdayText").innerHTML = localStorage.getItem('thursdayText');
+document.getElementById("fridayText").innerHTML = localStorage.getItem('fridayText');
+document.getElementById("saturdayText").innerHTML = localStorage.getItem('saturdayText');
+});
