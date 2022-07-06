361d807cbb0bc48b59f640fac72d2d112db918f4
Jane McPheron <jvmcpheron@Janes-MacBook-Pro.local>
2022-07-06 14:18:31 -0500

    successfully saves to local storage with save button

---
+++ script.js
@@ -0,0 +1,35 @@
+let sundayButton = document.getElementsByClassName('btn-success')[0];
+let mondayButton = document.getElementsByClassName('btn-success')[1];
+let tuesdayButton = document.getElementsByClassName('btn-success')[2];
+let wednesdayButton = document.getElementsByClassName('btn-success')[3];
+let thursdayButton = document.getElementsByClassName('btn-success')[4];
+let fridayButton = document.getElementsByClassName('btn-success')[5];
+let saturdayButton = document.getElementsByClassName('btn-success')[6];
+
+sundayButton.addEventListener('click',  function(){
+    localStorage.setItem('sundayText', document.getElementById("sundayText").value);
+});
+
+mondayButton.addEventListener('click',  function(){
+    localStorage.setItem('mondayText', document.getElementById("mondayText").value);
+});
+
+tuesdayButton.addEventListener('click',  function(){
+    localStorage.setItem('tuesdayText', document.getElementById("tuesdayText").value);
+});
+
+wednesdayButton.addEventListener('click',  function(){
+    localStorage.setItem('wednesdayText', document.getElementById("wednesdayText").value);
+});
+
+thursdayButton.addEventListener('click',  function(){
+    localStorage.setItem('thursdayText', document.getElementById("thursdayText").value);
+});
+
+fridayButton.addEventListener('click',  function(){
+    localStorage.setItem('fridayText', document.getElementById("fridayText").value);
+});
+
+saturdayButton.addEventListener('click',  function(){
+    localStorage.setItem('saturdayText', document.getElementById("saturdayText").value);
+});
